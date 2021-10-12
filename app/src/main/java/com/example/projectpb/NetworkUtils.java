1aa3115e3aa858dd8818415e9060a53b72d60900
rizapadlevi <rizapadlevi33@gmail.com>
2021-10-12 23:34:30 +0700

    NETWORKING

---
+++ app/src/main/java/com/example/projectpb/NetworkUtils.java
@@ -0,0 +1,11 @@
+package com.example.projectpb;
+
+public class NetworkUtils {
+    private static final String LOG_TAG =
+            NetworkUtils.class.getSimpleName();
+    
+    static void getMasakanInfo(String queryString){
+
+    }
+
+}
